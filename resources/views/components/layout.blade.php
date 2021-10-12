6554800a9f77f7b3f6022753be5d4d52d0195ffe
Eddy Rufino <eddyjaair@gmail.com>
2021-10-12 12:07:27 -0500

    delete avatar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -58,7 +58,7 @@
         </div>
 
         <footer id="newsletters" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
-            <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
+            {{-- <img src="/images/avatar.png" alt="" class="mx-auto -mb-6" style="width: 145px;"> --}}
             <h5 class="text-3xl font-normal">Mantente en contacto con las últimas publicaciones</h5>
             <p class="text-sm text-gray-700 mt-3">Recibirás correos sin publicidad.</p>
 
