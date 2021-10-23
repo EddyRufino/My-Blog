58256186c877fef7e08d149d53951165cb48baa0
Eddy Rufino <eddyjaair@gmail.com>
2021-10-22 23:08:25 -0500

    add favicon and refactorin copy link

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -41,6 +41,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Eddy Rufino</title>
+        <link rel="shortcut icon" href="{{ asset('images/cara.png') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Rubik:300,500,700" rel="stylesheet">
--- resources/views/components/social-media-share.blade.php
+++ resources/views/components/social-media-share.blade.php
@@ -1,22 +1,62 @@
 @props(['title', 'slug'])
 
 <style>
-input::selection {
-    background: white;
-    color: white;
-    text-shadow: 1px 1px #fff;
-}
+    input::selection {
+        background: white;
+        color: white;
+        text-shadow: 1px 1px #fff;
+    }
 
-/* Firefox */
-input::-moz-selection {
-    background: white;
-    color: white;
-    text-shadow: 1px 1px #fff;
-}
+    /* Firefox */
+    input::-moz-selection {
+        background: white;
+        color: white;
+        text-shadow: 1px 1px #fff;
+    }
 
+    .tooltip {
+      position: relative;
+      display: inline-block;
+    }
+
+    .tooltip .tooltiptext {
+      visibility: hidden;
+      width: 120px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      border-radius: 6px;
+      padding: 5px 0;
+      position: absolute;
+      z-index: 1;
+      bottom: 125%;
+      left: 50%;
+      margin-left: -60px;
+      opacity: 0;
+      transition: opacity 0.3s;
+    }
+
+    .tooltip .tooltiptext::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }
+
+    .tooltip:hover .tooltiptext {
+      visibility: visible;
+      opacity: 1;
+    }
+    .boton {
+        bottom: 100%;
+    }
 </style>
 
-<div class="max-w-xl mt-8 w-full flex">
+<div class="max-w-xl mt-8 w-full flex px-5">
     <p class="text-xl font-light mr-3">Compartir:</p>
     <ul class="flex mt-1">
         <li class="mr-3">
@@ -26,7 +66,7 @@ input::-moz-selection {
                 <x-icons.facebook></x-icons.facebook>
             </a>
         </li>
-        <li class="mr-1">
+        <li class="mr-2">
             <a href="https://twitter.com/intent/tweet?url={{ request()->fullUrl() }}&text=Creo que podría ayudarte este nuevo post: {{ $title }}&via=rufino_eddy&hashtags={{ config('app.name') }}"
                 target="_blank"
                 title="Compartir en twitter">
@@ -34,25 +74,27 @@ input::-moz-selection {
             </a>
         </li>
         <li class="copy">
-            <input type="text" class="w-0 p-0 border-0 focus:ring-2 focus:ring-transparent" value="https://eddyrufino.com/blog/{{ $slug }}">
-            <button type="button"><x-icons.copy></x-icons.copy></button>
+
+            <button id="boton" type="button" class="tooltip" >
+                <span id="tooltip" class="tooltiptext">Copia la url</span>
+                <x-icons.copy></x-icons.copy>
+            </button>
         </li>
     </ul>
 </div>
-
+<input id="caja" type="text" class="boton absolute p-0 border-0 focus:ring-2 focus:ring-transparent" value="https://eddyrufino.com/blog/{{ $slug }}">
 <script>
 (function() {
-    var copyButton = document.querySelector('.copy button');
-    var copyInput = document.querySelector('.copy input');
+    let copyButton = document.querySelector('.copy #boton');
+    let copyInput = document.querySelector('#caja');
+    let tooltip = document.querySelector('#tooltip');
     copyButton.addEventListener('click', function(e) {
         e.preventDefault();
-        var text = copyInput.select();
+        let text = copyInput.select();
         document.execCommand('copy');
-    });
-
-    copyInput.addEventListener('click', function() {
-        this.select();
-
+        // span.innerHTML = 'Url copiada';
+        tooltip.innerHTML = "Url copiada";
+        // console.log(span);
     });
 })();
 </script>
